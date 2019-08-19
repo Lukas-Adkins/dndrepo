c03ca91e632e6898fcd5b96d99b1a0700312474a
adkinsl4 <adkinsl4@wwu.edu>
2019-08-18 17:06:31 -0700

    Added more user input parsing

--- src/EncounterGenerator.java
+++ src/EncounterGenerator.java
@@ -38,6 +38,6 @@ public class EncounterGenerator {
     }
 
     enum Location{
-        Forest;
+        Forest, Arctic, Coast, Desert, Grassland, Hill, Mountain, Swamp, Underdark, Underwater, Urban
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        boolean running = true;
+        Scanner sc = new Scanner(System.in);
         ArrayList<Monster> monsterList= new ArrayList<>();
         MonsterListParser listParser = new MonsterListParser(new File("monsters.txt"));
         listParser.fillList(monsterList);
         EncounterGenerator generator = new EncounterGenerator(new File("exp.txt"));
-        generator.newEncounter(4,1,2, EncounterGenerator.Location.Forest);
+
+        System.out.println("Welcome to Goo's D&D Encounter generator. Please enter the number of players in your party:");
+        int players = sc.nextInt();
+        System.out.println("Next, please enter your party's level:");
+        int level = sc.nextInt();
+        System.out.println("To begin generating encounters, please enter a biome and difficultly.");
+        System.out.println("Example input 'Hard Coast' would generate a hard encounter taking place in a coast biome.");
+        System.out.println("Difficulties are Easy, Medium, Hard, or Deadly.");
+        System.out.println("Possible biomes are as follows: ");
+        System.out.println("Forest, Arctic, Coast, Desert, Grassland, Hill, Mountain, Swamp, Underdark, Underwater, or Urban.");
+
+        while(running){
+            //TODO add user input parsing
+            generator.newEncounter(players,level,2, EncounterGenerator.Location.Forest);
+        }
     }
 }
